9065bc164363401fae549f12808ea50ad645fe74
Tom de Bruin <tom@deadlyhifi.com>
2019-06-08 16:53:54 +0100

    Svelte up and running

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let name;
+  let count = 0;
+
+  const handleClick = () => {
+    count += 1;
+    console.log({ count });
+  };
+
+  const handleReset = () => (count = 0);
+</script>
+
+<style>
+  h1 {
+    font-weight: 100;
+    text-transform: capitalize;
+  }
+  strong {
+    font-weight: bold;
+  }
+  h2 {
+    font-weight: 100;
+    font-size: 1.2rem;
+  }
+</style>
+
+<h1>
+  Hello
+  <strong>{name}</strong>
+  !
+</h1>
+<h2>Playing with Svelte.</h2>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+<button on:click={handleReset} disabled={count === 0}>Reset</button>
